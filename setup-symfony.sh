5b6caf5bc96a8419d539c3ed125da7e176502ba5
root <root@localhost.localdomain>
2015-10-31 12:38:17 -0400

    setting env export SYMFONY_ENV=prod, before running the composer update

--- setup-symfony.sh
+++ setup-symfony.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p /var/www/ttapp
 cd /var/www/ttapp
+export SYMFONY_ENV=prod
 composer install --no-dev --optimize-autoloader --no-interaction
 /usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug
 chown -R nginx:nginx /var/www/ttapp/app/logs /var/www/ttapp/app/cache;
