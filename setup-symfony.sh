54b327fa14d71e785a9ce05a41f0e17f1b481fc7
Khizer Naeem <khizernaeem@gmail.com>
2015-10-31 15:07:40 -0400

    debuggin setup-symfony.sh for php55

--- setup-symfony.sh
+++ setup-symfony.sh
@@ -3,6 +3,6 @@ mkdir -p /var/www/ttapp
 cd /var/www/ttapp
 export COMPOSER_HOME="/root"
 export SYMFONY_ENV=prod
-/bin/composer install --no-dev --optimize-autoloader --no-interaction
-scl enable php55 '/usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug'
+/bin/composer install --no-dev --optimize-autoloader --no-interaction &>> /tmp/cd_debug
+scl enable php55 'php /var/www/ttapp/app/console cache:clear --env=prod --no-debug' &>> /tmp/cd_debug
 chown -R nginx:nginx /var/www/ttapp/app/logs /var/www/ttapp/app/cache; 
