b28d0c10b2c5e227952042a84e3d4f3ddbb1f790
Khizer Naeem <khizernaeem@gmail.com>
2015-10-31 15:01:12 -0400

    moved to php55

--- setup-symfony.sh
+++ setup-symfony.sh
@@ -4,5 +4,5 @@ cd /var/www/ttapp
 export COMPOSER_HOME="/root"
 export SYMFONY_ENV=prod
 /bin/composer install --no-dev --optimize-autoloader --no-interaction
-/usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug
+scl enable php55 '/usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug'
 chown -R nginx:nginx /var/www/ttapp/app/logs /var/www/ttapp/app/cache; 
