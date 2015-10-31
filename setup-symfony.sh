7f8fdf49e69e181a69f648c15d72fcda1344719d
root <root@localhost.localdomain>
2015-10-31 12:59:20 -0400

    removing debugging from setup-symfony.sh

--- setup-symfony.sh
+++ setup-symfony.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "$(date): I am: $(whoami)" >> /tmp/cd_debug
 mkdir -p /var/www/ttapp
 cd /var/www/ttapp
 export COMPOSER_HOME="/root"
 export SYMFONY_ENV=prod
-/bin/composer install --no-dev --optimize-autoloader --no-interaction &>> /tmp/cd_debug
-/usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug &>> /tmp/cd_debug
+/bin/composer install --no-dev --optimize-autoloader --no-interaction
+/usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug
 chown -R nginx:nginx /var/www/ttapp/app/logs /var/www/ttapp/app/cache; 
