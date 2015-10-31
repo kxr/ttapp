899fce16b7bb0a94ca4a9631f26f7a0bf224a34f
root <root@localhost.localdomain>
2015-10-31 12:45:05 -0400

    codedeploy afterinstall debuging

--- setup-symfony.sh
+++ setup-symfony.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo "$(date): I am: $(whoami) >> /tmp/cd_debug
 mkdir -p /var/www/ttapp
 cd /var/www/ttapp
 export SYMFONY_ENV=prod
-composer install --no-dev --optimize-autoloader --no-interaction
+/bin/composer install --no-dev --optimize-autoloader --no-interaction
 /usr/bin/php /var/www/ttapp/app/console cache:clear --env=prod --no-debug
 chown -R nginx:nginx /var/www/ttapp/app/logs /var/www/ttapp/app/cache;
