6b3197aaa5cb960b6b4b2784f85253ae00cadbb8
Khizer Naeem <khizernaeem@gmail.com>
2015-11-01 12:58:32 -0500

    working on SetupController.php

--- src/kxr/Bundle/TTAppBundle/Controller/SetupController.php
+++ src/kxr/Bundle/TTAppBundle/Controller/SetupController.php
@@ -5,17 +5,17 @@ namespace kxr\Bundle\TTAppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
-class SetupController extends Controller
-{
-    public function indexAction(Request $request)
-    {
+class SetupController extends Controller {
+
+    public function indexAction(Request $request) {
+
         $webhook_payload = $request->request->get('payload');
-	$head_commit_id = $webhook_payload['head_commit']['id'];
+	if ( !empty( $webhook_payload ) ) {
+		$payload_params = json_decode($webhook_payload, true);
+	}
         $logger = $this->get('logger');
-        $logger->error(print_r($head_commit_id, true));
+        $logger->error(print_r($payload_params, true));
         return new Response( 'nananna' );
 
 
