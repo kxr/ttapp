53800f984751bfff1fe06f7ad21cb42e8f684b80
Khizer Naeem <khizernaeem@gmail.com>
2015-11-01 12:16:16 -0500

    working on SetupController.php

--- src/kxr/Bundle/TTAppBundle/Controller/SetupController.php
+++ src/kxr/Bundle/TTAppBundle/Controller/SetupController.php
@@ -5,11 +5,17 @@ namespace kxr\Bundle\TTAppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 class SetupController extends Controller
 {
     public function indexAction(Request $request)
     {
-        return new Response( $request->request->get('test') );
+        $webhook_post = $request->request->get('test');
+	$fs = new Filesystem();
+	$fs->dumpFile('/tmp/setup_postdata', var_dump($webhook_post) );
+        return new Response( 'dadaadada' );
+
     }
 }
