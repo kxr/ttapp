9fb69a733685157dad444a0610b7079f34e832b3
Khizer Naeem <khizernaeem@gmail.com>
2015-11-01 12:45:51 -0500

    working on SetupController.php

--- src/kxr/Bundle/TTAppBundle/Controller/SetupController.php
+++ src/kxr/Bundle/TTAppBundle/Controller/SetupController.php
@@ -12,7 +12,7 @@ class SetupController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $webhook_post = $request->request->all();
+        $webhook_post = $request->request->get('payload');
         $logger = $this->get('logger');
         $logger->error(print_r($webhook_post, true));
         return new Response( 'nananna' );
