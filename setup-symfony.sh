c7704376669d3691a3eebdbe09c82add4c038f13
kxr <khizernaeem@gmail.com>
2015-11-07 19:47:47 +0400

    Adding the ilb dns names in parameters.yml when setup-symfony is called

--- setup-symfony.sh
+++ setup-symfony.sh
@@ -4,8 +4,8 @@ ELB_DBAM_DNS=$(aws elb describe-load-balancers --load-balancer-names ilb-dbam | 
 ELB_DBNZ_DNS=$(aws elb describe-load-balancers --load-balancer-names ilb-dbnz | jq -r '.LoadBalancerDescriptions[].DNSName')
 
 cd /var/www/ttapp
-sed -i "s/ilb-dbam-UPDATEME/$ELB_DBAM_DNS/"
-sed -i "s/ilb-dbnz-UPDATEME/$ELB_DBNZ_DNS/"
+sed -i "s/ilb-dbam-UPDATEME/$ELB_DBAM_DNS/" app/config/parameters.yml.dist
+sed -i "s/ilb-dbnz-UPDATEME/$ELB_DBNZ_DNS/" app/config/parameters.yml.dist
 
 export COMPOSER_HOME="/root"
 export SYMFONY_ENV=prod
