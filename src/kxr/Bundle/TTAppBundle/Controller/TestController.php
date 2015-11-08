15da371694762ffb3d7466600256d0813c73116e
kxr <khizernaeem@gmail.com>
2015-11-08 20:14:16 +0400

    Generating random first/last names

--- src/kxr/Bundle/TTAppBundle/Controller/TestController.php
+++ src/kxr/Bundle/TTAppBundle/Controller/TestController.php
@@ -10,17 +10,12 @@ class TestController extends Controller
     public function indexAction()
     {
 	// Just for the fun of it, lets generate a random Pronounceable name instead of just random noise
-	$titles = array ( "Mr", "Miss", "Mrs", "Dr", "Engg", "Master", "Sir", "Lord", "King", "Queen", "Prince", "Princess", "Emperor", "Empress", "Sheikh" );
+	$titles = array ( "Mr", "Miss", "Mrs", "Dr", "Engg", "Master", "Sir", "Lord", "King", "Queen", "Prince", "Princess", "Emperor", "Empress", "Sheikh", "Muhammad" );
 	$vowels = array ( "a", "e", "i", "o", "u");
 	$constants = array ( "b", "bl", "br", "c", "ch", "chr", "cl", "cr", "d", "dl", "dr", "f", "fl", "fr", "g", "gh", "gl", "gr", "h", "hr", "j", "jr", "k", "kh", "kl", "kr", "l", "ld", "lr", "m", "mr", "n", "ng", "nr", "p", "ph", "pl", "pr", "qu", "r", "rl", "rn", "s", "sh", "sl", "str", "t", "th", "tr", "v", "vl", "w", "wr", "x", "y", "z", "zr" );
 $endings = array( "r", "n", "l", "", "", "", "" );
-	$ran_name = 	$titles[array_rand($titles,1)] .
-			" " .
-			ucfirst(        $constants[array_rand($constants,1)] .
-					$vowels[array_rand($vowels,1)] .
-					$constants[array_rand($constants,1)] .
-					$vowels[array_rand($vowels,1)] .
-					$endings[array_rand($endings,1)] ) .
+
+	$first_name = 	$titles[array_rand($titles,1)] .
 			" " .
 			ucfirst(        $constants[array_rand($constants,1)] .
 					$vowels[array_rand($vowels,1)] .
@@ -28,6 +23,14 @@ $endings = array( "r", "n", "l", "", "", "", "" );
 					$vowels[array_rand($vowels,1)] .
 					$endings[array_rand($endings,1)] );
 
-        return new Response( $ran_name );
+	$last_name =	ucfirst(        $constants[array_rand($constants,1)] .
+					$vowels[array_rand($vowels,1)] .
+					$constants[array_rand($constants,1)] .
+					$vowels[array_rand($vowels,1)] .
+					$constants[array_rand($constants,1)] .
+					$vowels[array_rand($vowels,1)] .
+					$endings[array_rand($endings,1)] );
+
+        return new Response( 'First Name: ' . $first_name . 'Last Name: ' . $last_name );
     }
 }
