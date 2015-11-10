da9b6674bb821ed22a3599759d246ba8dddc82f8
kxr <khizernaeem@gmail.com>
2015-11-10 13:08:40 +0400

    working on takedown endpoint

--- src/kxr/Bundle/TTAppBundle/Controller/TakedownController.php
+++ src/kxr/Bundle/TTAppBundle/Controller/TakedownController.php
@@ -19,63 +19,47 @@ class TakedownController extends Controller
 		'version' => 'latest',
 		'region' => 'us-east-1'
 	]);
-	$pg1 = $Ec2Client->DescribeInstances( array(
-		'Filters' => array(
-			array(
-				'Name' => 'tag:Name',
-				'Values' => array('pg1'),
-			)
-		)
-	) );
-	$pg1_ip = $pg1['Reservations']['0']['Instances']['0']['PrivateIpAddress'];
-	$pg2 = $Ec2Client->DescribeInstances( array(
-		'Filters' => array(
-			array(
-				'Name' => 'tag:Name',
-				'Values' => array('pg2'),
-			)
-		)
-	) );
-	$pg1_ip = $pg2['Reservations']['0']['Instances']['0']['PrivateIpAddress'];
-
-	$pgdb_status=""
-	//pg1->dbam
-	$dbconn = pg_connect("host=$pg1_ip port=5432 user=monuser dbname=postgres connect_timeout=5")
-	if (!$dbconn) {
-		$pgdb_status .= "PG1-DBAM: DOWN"
-	}
-	else {
-		$res = pg_query($dbconn, 'select pg_is_in_recovery()');
-		$pgdb_status .= "PG1-DBAM: ".print_r($res, true);
-	}
-	//pg1->dbnz
-	$dbconn = pg_connect("host=$pg1_ip port=5433 user=monuser dbname=postgres connect_timeout=5")
-	if (!$dbconn) {
-		$pgdb_status .= "PG1-DBNZ: DOWN"
-	}
-	else {
-		$res = pg_query($dbconn, 'select pg_is_in_recovery()');
-		$pgdb_status .= "PG1-DBNZ: ".print_r($res, true);
-	}
-	//pg2->dbam
-	$dbconn = pg_connect("host=$pg2_ip port=5432 user=monuser dbname=postgres connect_timeout=5")
-	if (!$dbconn) {
-		$pgdb_status .= "PG2-DBAM: DOWN"
-	}
-	else {
-		$res = pg_query($dbconn, 'select pg_is_in_recovery()');
-		$pgdb_status .= "PG2-DBAM: ".print_r($res, true);
-	}
-	//pg1->dbnz
-	$dbconn = pg_connect("host=$pg2_ip port=5433 user=monuser dbname=postgres connect_timeout=5")
-	if (!$dbconn) {
-		$pgdb_status .= "PG2-DBNZ: DOWN"
-	}
-	else {
-		$res = pg_query($dbconn, 'select pg_is_in_recovery()');
-		$pgdb_status .= "PG2-DBNZ: ".print_r($res, true);
+	// name => instanceid
+	$nodes = ['pg1' => '' , 'pg2' => ''];
+	// db => port
+	$dbs = array( 'dbam' => '5432', 'dbnz' => '5433' );
+	// status
+	$status = [];
+	// Iterate through each node
+	foreach ( $nodes as $node => $iid ){
+		$status[$node] = [];
+		// Get node IP
+		$ec2c = $Ec2Client->DescribeInstances( array(
+			'Filters' => array(
+				array(
+					'Name' => 'tag:Name',
+					'Values' => array($node) ) ) ) );
+		$ip = $ec2c['Reservations']['0']['Instances']['0']['PrivateIpAddress'];
+		// While we are at it, populate the instance id
+		$nodes[$node] = $ec2c['Reservations']['0']['Instances']['0']['InstanceId'];
+		// Iterate through each db
+		foreach ( $dbs as $db => $dbport ) {
+			$status[$node][$db] = '';
+			$dbconn = pg_connect("host=$ip port=$dbport user=monuser dbname=postgres connect_timeout=10");
+			if (!$dbconn)
+				$status[$node][$db] = 'DOWN';
+			else {
+				$res = pg_query($dbconn, 'select pg_is_in_recovery()');
+				$stat = pg_fetch_assoc($res);
+				if ( $stat['pg_is_in_recovery'] == 't' )
+					$status[$node][$db] = 'SLAVE';
+				elseif ( $stat['pg_is_in_recovery'] == 'f' )
+					$status[$node][$db] = 'MASTER';
+				else
+					$status[$node][$db] = 'UNKNOWN';
+			}
+			
+		}
 	}
 
-	return new Response( $pgdb_status );
+	
+
+
+	return new Response( print_r($status, true).print_r($nodes, true) );
     }
 }
