b31b56caa5a36c922c58ab773ac578b64e017adc
kxr <khizernaeem@gmail.com>
2015-11-11 21:07:24 +0400

    Removing app bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            //new AppBundle\AppBundle(),
             new \Aws\Symfony\AwsBundle(),
             new kxr\Bundle\TTAppBundle\kxrTTAppBundle(),
         );
