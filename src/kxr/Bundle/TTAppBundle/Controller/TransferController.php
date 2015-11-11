e8ce9130582d6f794a16c8255e4e9b98fd2a10f5
kxr <khizernaeem@gmail.com>
2015-11-11 20:48:00 +0400

    finalizing transfer endpoint

--- src/kxr/Bundle/TTAppBundle/Controller/TransferController.php
+++ src/kxr/Bundle/TTAppBundle/Controller/TransferController.php
@@ -11,7 +11,6 @@ class TransferController extends Controller
 {
     public function indexAction()
     {
-
 	// Connect to the Mongodb database
 	$Mongo = new MongoClient( 'mongodb://'.
 					$this->getParameter('mongodb_server').
@@ -22,16 +21,6 @@ class TransferController extends Controller
 		return new Response('Error connecting to mongodb');
 	$Collection = $Mongo->ttdb->users;
 
-	// Create connection to the Postgresql databases
-	#$conn_dbam = $this->getDoctrine()->getRepository('kxrTTAppBundle:Users');
-	#$conn_dbnz = $this->getDoctrine()->getRepository('kxrTTAppBundle:Users');
-	$conn_dbam = $this->get('doctrine.dbal.dbam_connection');
-	$conn_dbnz = $this->get('doctrine.dbal.dbnz_connection');
-
-	// Get the last entry's id of our users table from both databases
-	$dbam_lastent = $conn_dbam->fetchAssoc('SELECT MAX(id) FROM users')['max'];
-	$dbnz_lastent = $conn_dbnz->fetchAssoc('SELECT MAX(id) FROM users')['max'];
-
 	// Copy rows in batches to Mongodb to
 	// Keep the memory foot print per query low
 	//
@@ -40,39 +29,68 @@ class TransferController extends Controller
 	// en/latest/reference/batch-processing.html#iterating-large-results-for-data-processing
 	// I'll try to accomplish that with postgresql's scroll feature
 	$cursor_batch_size = 1000; // 1000 records assoc array ~= 162KB
+	// Some variables to report
+	$counter = [	'dbs_to_process'	=> [],
+			'last_entry'		=> ['dbam'=>0,'dbnz'=>0],
+			'rows_count' 		=> ['dbam'=>0,'dbnz'=>0],
+			'batch_iterations'	=> ['dbam'=>0,'dbnz'=>0],
+			'copy_time'		=> ['dbam'=>0,'dbnz'=>0],
+			'delete_time'		=> ['dbam'=>0,'dbnz'=>0]		];
 
-	$count=1;
-	$conn_dbam->beginTransaction();
-	$conn_dbam->executeQuery( "SET transaction_read_only=true" );
-	$conn_dbam->executeQuery( "DECLARE cur NO SCROLL CURSOR FOR (SELECT * from users where id <= $dbam_lastent)" );
-	do {
-		$cursor_batch = $conn_dbam->fetchAll( "FETCH $cursor_batch_size FROM cur" );
-		$cursor_count = count($cursor_batch);
-		if ( $cursor_count > 0 ) {
-			$Collection->batchInsert( $cursor_batch );
-			$count ++;
+	// Create database connections,
+	// Fetch the latest entry ids from both the database,
+	// and check if we have any thing to process
+	$conn=[];
+	foreach ( ['dbam', 'dbnz'] as $db ) {
+		$conn[$db] = $this->get('doctrine.dbal.'.$db.'_connection');
+		// Get the last entry's id of our users table from both databases
+		$counter['last_entry'][$db] = $conn[$db]->fetchAssoc('SELECT MAX(id) FROM users')['max'];
+		if ( $counter['last_entry'][$db] ) {
+			array_push( $counter['dbs_to_process'], "$db" );
 		}
-	} while ( $cursor_count > 0 );
-
+	}
+	if ( count( $counter['dbs_to_process'] )  > 0 ) {
+		foreach ( $counter['dbs_to_process'] as $db ) {
+			// Begin transaction block
+			$conn[$db]->beginTransaction();
+			// Create the cursor
+			$conn[$db]->executeQuery( "DECLARE cur NO SCROLL CURSOR FOR (SELECT * FROM users WHERE id <= ".$counter['last_entry'][$db].")" );
+			$timer = microtime(true);
+			do {
+				// Fetch batch_size rows from the cursor
+				$cursor_batch = $conn[$db]->fetchAll( "FETCH $cursor_batch_size FROM cur" );
+				// Directly batch-insert them into mongodb
+				$cursor_count = count($cursor_batch);
+				if ( $cursor_count > 0 ) {
+					$Collection->batchInsert( $cursor_batch );
+					$counter['rows_count'][$db] += $cursor_count;
+					$counter['batch_iterations'][$db] += 1;		
+				}
+			} while ( $cursor_count > 0 );
+			$conn[$db]->executeQuery( "CLOSE cur" );
+			//$conn[$db]->commit();
+			$counter['copy_time'][$db] = microtime(true) - $timer;
 	
-
-
-
-
-
-
-	// Delete the copied rows from pgsql
-
-
-
-	//$Coll->batchInsert([
-	//	[ 'BatchInsert' => 'aaa', 'adfas' => 'adfas', '232' => 'adfas' ],
-	//	[ 'BatchInsert' => 'xxx', 'adfas' => 'adfas', '232' => 'adfas' ],
-	//	[ 'BatchInsert' => 'ggg', 'adfas' => 'adfas', '232' => 'adfas' ]
-	//]);
-
-
-	return new Response("iterations: ".$count);
-
+			// Bulk delete the rows from pgsql
+			$timer = microtime(true);
+			$conn[$db]->executeQuery('DELETE FROM users WHERE  id <= '.$counter['last_entry'][$db] );
+			$conn[$db]->commit();
+			$counter['delete_time'][$db] = microtime(true) - $timer;
+		}	
+	
+		return new Response(	"<b>Transaction Summary:</b>".
+					"<pre>".
+					"cursor_batch_size => ".$cursor_batch_size.
+					"</pre><pre>".
+					print_r( $counter, true ).
+					"</pre>"
+				);
+	}
+	else
+		return new Response( "<b>Didn't find any record in the databases to process</b>".
+					"<pre>".
+					print_r( $counter, true ).
+					"</pre>"
+				);
    }
 }
